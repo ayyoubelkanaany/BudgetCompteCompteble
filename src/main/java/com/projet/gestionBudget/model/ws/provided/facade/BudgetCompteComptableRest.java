f4d2a734b1fb0d085fc0aedc8a651cfd3d71e387
ayyoub <elkanaany.21@gmail.com>
2021-02-03 21:57:22 +0100

    seconde commit

--- src/main/java/com/projet/gestionBudget/model/ws/provided/facade/BudgetCompteComptableRest.java
+++ src/main/java/com/projet/gestionBudget/model/ws/provided/facade/BudgetCompteComptableRest.java
@@ -3,6 +3,7 @@ package com.projet.gestionBudget.model.ws.provided.facade;
 import java.util.List;
 import com.projet.gestionBudget.model.ws.provided.vo.BudgetCompteComptableVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
